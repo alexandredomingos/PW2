7bbb99ddb6741a7177c23bac7f8155467c9937a4
alexandredomingos <alexandredomingos@ipvc.pt>
2023-03-25 22:55:11 +0000

    Commit

--- pontos-turisticos/index.php
+++ pontos-turisticos/index.php
@@ -25,27 +25,26 @@
             <div class="col">
               <div class='row row-cols-1 row-cols-md-3 g-4'>
                 <div class="card topic" style=" width: 20rem;">
-                  <img src="../assets/images/turistico/antiga-casa-da-camara.jpg" class="card-img-top" alt="foto perfil" style="width: 320px;height: 320px;">
+                  <img src="../assets/images/turistico/antiga-casa-da-camara.jpg" class="card-img-top" alt="Casa Da Camara" style="width: 320px;height: 320px;">
                   <div class="card-body">
                     <h5 class="card-title">Antiga Casa da Camara</h5>
-                    <p class="card-text">Descr</p>
-                   <a href='#' class='btn btn-success'>Ver Mais</a>
+                    
+                   <a href='antiga-casa-da-camara' class='btn btn-success'>Ver Mais</a>
                   </div>
                 </div>
                 <div class="card topic" style=" width: 20rem;">
-                  <img src="../assets/images/turistico/campo-militar-batalha-trancoso.jpg" class="card-img-top" alt="foto perfil" style="width: 320px;height: 320px;">
+                  <img src="../assets/images/turistico/campo-militar-batalha-trancoso.jpg" class="card-img-top" alt="Campo Batalha Trancoso" style="width: 320px;height: 320px;">
                   <div class="card-body">
                     <h5 class="card-title">Campo Militar da Batalha de Trancoso</h5>
-                    <p class="card-text">Descr</p>
-                   <a href='#' class='btn btn-success'>Ver Mais</a>
+                    
+                   <a href='campo-militar-trancoso' class='btn btn-success'>Ver Mais</a>
                   </div>
                 </div>
                 <div class="card topic" style=" width: 20rem;">
-                  <img src="" class="card-img-top" alt="foto perfil" style="width: 320px;height: 320px;">
+                  <img src="../assets/images/turistico/castelo.jpg" class="card-img-top" alt="Castelo Trancoso" style="width: 320px;height: 320px;">
                   <div class="card-body">
-                    <h5 class="card-title">Nome</h5>
-                    <p class="card-text">Descr</p>
-                   <a href='#' class='btn btn-success'>Ver Mais</a>
+                    <h5 class="card-title">Castelo</h5>
+                   <a href='castelo' class='btn btn-success'>Ver Mais</a>
                   </div>
                 </div>
                 <div class="card topic" style=" width: 20rem;">
