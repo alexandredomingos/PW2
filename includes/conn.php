0623d6fc6b7ed8e71eab5109fae9ea6f45304861
alexandredomingos <alexandredomingos@ipvc.pt>
2023-05-06 09:02:31 +0100

    loginformupdate

---
+++ includes/conn.php
@@ -0,0 +1,6 @@
+<?php
+$con = mysqli_connect("localhost","root","","trancoso_aldeia");
+//$con = mysqli_connect("localhost","aldeia_trancoso","!bnbTWiZN)%5SV/m","trancoso_aldeia");
+
+
+?>
---
+++ login/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Sobre Trancoso</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body class="container"  style="background-image: url('../assets/images/trancoso1.webp');">
+    <div id="containerlogin" class="wraplogin p-t-30 p-b-50">
+        <span class="logintitle p-b-41">
+        Iniciar Sessão
+        </span>
+            <div class="col-sm-12">
+                <form class="login100-form validate-form p-b-33 p-t-5">
+                    <div class="wrap-inputform validate-input" data-validate="Enter username">
+                        <input class="inputform" type="text" name="username" placeholder="E-mail" autocomplete="off">
+                        <span class="focusinput" data-placeholder="@"></span>
+                    </div>
+                    <div class="wrap-inputform validate-input" data-validate="Enter password">
+                        <input class="inputform" type="password" name="pass" placeholder="Password" autocomplete="off">
+                        <span class="focusinput" data-placeholder="*"></span>
+                    </div>
+                    <div class="formbtn m-t-32">
+                        <button class="loginbtn">
+                        Login
+                        </button>
+                    </div>
+            </form>
+        </div>
+    </div>
+
+    
+</body>
+</html>
